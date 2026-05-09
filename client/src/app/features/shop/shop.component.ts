50a70f097a2d058997a61d8fbfa8b55c722e6f91
javedkhan1989 <jksoft2010@gmail.com>
2026-05-09 23:32:43 +0530

    End of Section 9

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -5,22 +5,36 @@ import { MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
 import { MatDialog } from '@angular/material/dialog';
 import { FiltersDialogComponent } from './filters-dialog/filters-dialog.component';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { MatMenu, MatMenuTrigger } from '@angular/material/menu';
+import { MatListOption, MatSelectionList, MatSelectionListChange } from '@angular/material/list';
+import { RouterLinkActive } from "@angular/router";
+import { ShopParams } from '../../shared/models/shopParams';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { pagination } from '../../shared/models/pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [MatCard, ProductItemComponent,MatButton,MatIcon],
+  imports: [MatCard, ProductItemComponent, MatButton, MatIcon, MatMenu, MatSelectionList, MatListOption, MatMenuTrigger, RouterLinkActive, MatPaginator, FormsModule, MatIconButton],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss',
 })
 export class ShopComponent implements OnInit {
   private shopService = inject(ShopService);
   private dialogService = inject(MatDialog);
-  products: Product[]=  [];
-  selectedBrands: string[] = [];
-  selectedTypes: string[] = [];
+  products?: pagination<Product>;
+  
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'},
+  ]
+
+  shopParams=new ShopParams();
+  pageSizeOptions=[5,10,15,20];
 
   ngOnInit(): void {
     this.initializeShop();
@@ -29,27 +43,53 @@ export class ShopComponent implements OnInit {
   initializeShop(){
     this.shopService.getBrands();
     this.shopService.getTypes();
+    this.getProducts();
 
-    this.shopService.getProducts().subscribe({
-      next: response => this.products=response.data,
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => this.products=response,
       error: error => console.log(error)      
     })
+  }
+
+  getProducts(){
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => this.products=response,
+      error: error => console.log(error)
+    })
+  }
+
+  onSearchChange(){
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+
+  handlePageEvent(event: PageEvent){
+    this.shopParams.pageNumber = event.pageIndex + 1;
+    this.shopParams.pageSize = event.pageSize;
+    this.getProducts();
+  }
+
+  onSortChange(event: MatSelectionListChange){
+    const selectedOption = event.options[0];
+    if(selectedOption){
+      this.shopParams = selectedOption.value;      
+      this.shopParams.pageNumber = 1;
+      this.getProducts();
+    }    
   }
 
   openFilterDialog(){
    const dialogRef = this.dialogService.open(FiltersDialogComponent,{
       minWidth: '500px',
-      data: {selectedBrands: this.selectedBrands, selectedTypes: this.selectedTypes}      
+      data: {selectedBrands: this.shopParams.brands, selectedTypes: this.shopParams.types}      
     });
     dialogRef.afterClosed().subscribe({
       next: result => {
         if(result){          
-          this.selectedBrands = result.selectedBrands;
-          this.selectedTypes = result.selectedTypes;
-          this.shopService.getProducts(this.selectedBrands,this.selectedTypes).subscribe({
-            next: response => this.products=response.data,
-            error: error => console.log(error)
-          })
+          this.shopParams.brands = result.selectedBrands;
+          this.shopParams.types = result.selectedTypes;
+          this.shopParams.pageNumber = 1;
+          this.getProducts();
         }
       }
     })
