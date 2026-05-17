ee8307d2189e550d249768d9ece946dbcab7e80f
javedkhan1989 <jksoft2010@gmail.com>
2026-05-17 18:40:00 +0530

    End of Section 12

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,9 @@ using API.Errors;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Data.SeedData;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,8 +19,15 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connString = builder.Configuration.GetConnectionString("Redis")
+     ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString,true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+builder.Services.AddSingleton<ICartService,CartService>();
 
 var app = builder.Build();
 
